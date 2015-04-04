aca209a9d43c01e368457169195d872ac07739ee
Abraham Epton <abraham.epton@gmail.com>
2015-04-04 10:19:26 -0500

    Fixing bugs in precincts

--- chi_elections/precincts.py
+++ chi_elections/precincts.py
@@ -23,7 +23,7 @@ def get_precincts_for_ward(ward):
             continue
         if precinct:
             precincts.append(precinct)
-        precinct = {'ward': int(ward)}
+        precinct = {}
         for td_idx, td in enumerate(tr.xpath('td')):
             if tr_idx == 1:
                 # This is the header row
@@ -33,7 +33,7 @@ def get_precincts_for_ward(ward):
                     # We're done, so exit
                     return precincts
                 else:
-                    precinct['Pct'] = int(td.text_content().strip())
+                    precinct['id'] = '%02d%03d' % (ward, int(td.text_content().strip()))
             elif headers[td_idx] == '%':
                 continue
             else:
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     precincts = []
     for ward in range(1, 51):
         precincts.extend(get_precincts_for_ward(ward))
-    print precincts
+    print json.dumps(precincts)
